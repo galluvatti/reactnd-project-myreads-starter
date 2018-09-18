22a966c08d732484aebb20c1111bc271eea3861e
Giovanni Alluvatti <giovanni.alluvatti@lastminute.com>
2018-09-18 14:08:56 +0200

    implemented api call to retrieve all books and to search books

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom' 
+import {Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends Component {
 
-	render() {
-		return (
-			<div className="search-books">
-	            <div className="search-books-bar">
-	              <Link to="/" className="close-search">Close</Link>
-	              
-	              <div className="search-books-input-wrapper">
-	                {/*
+    state = {
+        query: '',
+        availableBooks: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            console.log(books)
+            this.setState({availableBooks: books})
+        })
+    }
+
+    updateQuery = (value) => {
+        let queryString = value.trim();
+        if (queryString.length > 0) {
+            BooksAPI.search(queryString).then((books) => {
+                console.log(books)
+                this.setState({availableBooks: books})
+            })
+        }
+        this.setState({query: queryString})
+    }
+
+    render() {
+        const {query} = this.state;
+
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">Close</Link>
+
+                    <div className="search-books-input-wrapper">
+                        {/*
 	                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
 	                  You can find these search terms here:
 	                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -18,16 +44,21 @@ class SearchBooks extends Component {
 	                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
 	                  you don't find a specific author or title. Every search is limited by search terms.
 	                */}
-	                <input type="text" placeholder="Search by title or author"/>
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
 
-	              </div>
-	            </div>
-	            <div className="search-books-results">
-	              <ol className="books-grid"></ol>
-	            </div>
-	        </div>
-          )
-	}
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SearchBooks
